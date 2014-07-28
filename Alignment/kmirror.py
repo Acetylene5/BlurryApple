bfc650525155db0695078f5d0e282aa77eb95962
Casey Deen <deen@mpia.de>
2014-07-28 13:54:36 +0200

    successfully calculated intersection!

--- Alignment/kmirror.py
+++ Alignment/kmirror.py
@@ -11,6 +11,7 @@ class Point( object ):
 class Line( object):
     def __init__(self, p, slope):
         norm = numpy.sqrt(slope[0]**2.0 + slope[1]**2.0 + slope[2]**2.0)
+        self.slope = slope/norm
         self.a = slope[0]/norm
         self.b = slope[1]/norm
         self.c = slope[2]/norm
@@ -26,16 +27,82 @@ class Line( object):
 class Plane( object ):
     def __init__(self, p, normal):
         self.p = p
-        self.normal = normal
+        self.normal = normal/numpy.sqrt(numpy.sum(numpy.square(normal)))
+        self.calculatePlaneEqn()
+
+    def calculatePlaneEqn(self):
+        Ax = self.normal[0]
+        Ay = self.normal[1]
+        Az = self.normal[2]
+        if (Ax == Ay == 0.0):
+            self.coeffs = numpy.array([0.0, 0.0, 1.0/self.p.z])
+        elif (Ax == Az == 0.0):
+            self.coeffs = numpy.array([0.0, 1.0/self.p.y, 0.0])
+        elif (Ay == Az == 0.0):
+            self.coeffs = numpy.array([1.0/self.p.x, 0.0, 0.0])
+        elif (Ax == 0):
+            p2 = Point(self.p.x, self.p.y+1.0, self.p.z-Ay/Az)
+            X = numpy.array([
+                [self.p.y, self.p.z],
+                [p2.y, p2.z]])
+            Y = numpy.array([1.0, 1.0])
+            BC = numpy.linalg.solve(X, Y)
+            self.coeffs = numpy.array([0.0, BC[0], BC[1]])
+        elif (Ay == 0):
+            p2 = Point(self.p.x+1.0, self.p.y, self.p.z-Ax/Az)
+            X = numpy.array([
+                [self.p.x, self.p.z],
+                [p2.x, p2.z]])
+            Y = numpy.array([1.0, 1.0])
+            AC = numpy.linalg.solve(X, Y)
+            self.coeffs = numpy.array([AC[0], 0.0, AC[1]])
+        elif (Az == 0):
+            p2 = Point(self.p.x-Ay/Ax, self.p.y-1.0, self.p.z)
+            X = numpy.array([
+                [self.p.x, self.p.y],
+                [p2.x, p2.y]])
+            Y = numpy.array([1.0, 1.0])
+            AB = numpy.linalg.solve(X,Y)
+            self.coeffs = numpy.array([AB[0], AB[1], 0.0])
+        else:
+            p2 = Point(self.p.x, self.p.y+1.0, self.p.z-Ay/Az)
+            p3 = Point(self.p.x+1.0, self.p.y, self.p.z-Ax/Az)
+            p4 = Point(self.p.x-Ay/Ax, self.p.y+1.0, self.p.z)
+            X = numpy.array([
+                [p2.x, p2.y, p2.z],
+                [p3.x, p3.y, p3.z],
+                [p4.x, p4.y, p4.z]])
+            Y = numpy.array([1.0, 1.0, 1.0])
+            self.coeffs = numpy.linalg.solve(X, Y)
 
     def intersection(self, line):
         dotproduct = numpy.dot(self.normal, line.slope)
-        if dotproduct = 0.0:
+        print("%3.2f" % dotproduct)
+        if dotproduct == 0.0:
             return False
+        else:
+            D = (self.coeffs[0]*line.p.x + self.coeffs[1]*line.p.y +
+                    self.coeffs[2]*line.p.z)
+            t = (1.0-D)/(self.coeffs[0]*line.a + self.coeffs[1]*line.b +
+                    self.coeffs[2]*line.c)
+            return line.traverse(t)
+
+    def reflection(self, line):
+        point,  = self.intersection(line)
+        if point:
+            newLine = Line(point, slope)
+        else:
+            print "Error! Line does not intersect plane!"
+
         
 
 origin = Point(0.0, 0.0, 0.0)
+p1 = Point(1.0, 2.45, 3.24)
+
 
 l1 = Line(origin, [1.0, 0.0, 0.0])
+plane1 = Plane(p1, [1.0, 1.0, 0.0])
 
-np = l1.traverse(2.0)
+print p1.x*plane1.coeffs[0]+p1.y*plane1.coeffs[1]+p1.z*plane1.coeffs[2]
+
+intersec = plane1.intersection(l1)
